a67a8f8fd0840e033f4f0e3cb7aabe30de5bb9e6
saya <moyashipoid@gmail.com>
2021-08-10 04:17:35 +0000

    トップページ作成用

--- resources/views/admin/mypage/index.blade.php
+++ resources/views/admin/mypage/index.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.base')
 
-        <title>Mypage</title>
-    </head>
-    <body>
-        <h1>Mypage</h1>
-    </body>
-</html>
+@section('title', 'マイページ')
+
+@section('content')
+<h1>mypage</h1>
+
+@endsection
