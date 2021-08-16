c9eede86c7edab9356e16fb203dc2affd947ef5f
saya <moyashipoid@gmail.com>
2021-08-16 09:50:31 +0000

    画像

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,6 +1,7 @@
 <header>
     <div class="container">
     <div class="header-title-area">
+        <img src="C:/Users/moyas/Downloads/チケット.png" class="ticket-image">
       <h1 class="logo">TICKET</h1>
     </div>
         <ul class="header-navigation">
@@ -10,10 +11,11 @@
             <li><a href="#">MYPAGE</a></li>
             <li><a href="#">会員登録</a></li>
         </ul>
-        <form class="header-navigation2">
+        <form class="header-serch">
             <input type="text">
             <input type="radio">映画
             <input type="radio">テーマパーク
-        </form>    
+        </form>
+        <img src="C:/Users/moyas/Downloads/movie-theater.png" class="ticket-banner">
   </div>
 </header>
